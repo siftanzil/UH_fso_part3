4c22c666c8591ef7f449e1281630f3998034eff4
siftanzil <siftanzil@gmail.com>
2023-06-19 16:43:12 +0600

    part3 3.1 done

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+
+// phonebook data
+
+let phonebook = [
+   {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456",
+   },
+   {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+   },
+   {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345",
+   },
+   {
+      id: 4,
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+   },
+   {
+      id: 5,
+      name: "Mars Poppendieck",
+      number: "38-23-6423122",
+   },
+];
+
+// log numbers in console
+
+phonebook.forEach((element) => {
+   console.log(element.id, element.name, element.number);
+});
+
+// use express for HTTP requests
+
+app.get("/", (req, res) => {
+   res.send(phonebook);
+});
+
+app.get("/api/persons", (req, res) => {
+   res.json(phonebook);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+   console.log(`server is running on port ${PORT}`);
+});
